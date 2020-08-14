6556feea2a9d69693bdb17a0e9d9ac2f128c9346
ubinexy <ubinexy@gmail.com>
2020-08-14 14:02:11 +0800

    [shiqi] initial code

---
+++ src/main/java/com/thoughtworks/rslist/MyShopApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.rslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopApplication.class, args);
+    }
+
+}
