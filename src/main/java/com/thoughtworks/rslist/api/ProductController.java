5d9cd4d5a33a1ebbf6bd7536bd5e779405136238
ubinexy <ubinexy@gmail.com>
2020-08-15 01:23:34 +0800

    [shiqi][Task1] fix: add missing imageUrl in getList controller, and add a cors origin

--- src/main/java/com/thoughtworks/rslist/api/ProductController.java
+++ src/main/java/com/thoughtworks/rslist/api/ProductController.java
@@ -5,6 +5,7 @@ import com.thoughtworks.rslist.dto.ProductDto;
 import com.thoughtworks.rslist.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +17,16 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @CrossOrigin(origins = "http://localhost:3001")
     @GetMapping("/api/products")
     ResponseEntity<List<Product>> getProductList() {
-
+        List<ProductDto> products_ = productRepository.findAll();
         List<Product> products = productRepository.findAll().stream().map(item->
                 Product.builder()
                 .name(item.getName())
                 .price(item.getPrice())
                 .unit(item.getUnit())
+                .imageUrl(item.getImageUrl())
                 .build())
         .collect(Collectors.toList());
         return ResponseEntity.ok(products);
